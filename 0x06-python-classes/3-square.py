92fe8634470fd4372aba0cbcd8798535def4771c
Bor3y9 <yousef161247@eng.psu.edu.eg>
2023-10-25 22:23:03 +0300

    my first new method

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""Square module.
+This module contains a class that defines a square with its size.
+"""
+
+
+class Square:
+    """represents a square"""
+    def __init__(self, size=0):
+        """Initializes a new Square.
+
+        Args:
+            size (int): The size of the new square.
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        """A method to calculate the area of square"""
+        area = self.__size * self.__size
+        return area
