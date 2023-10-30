7dfc1af5ea65cfb6a61b3f5bbb4a60a22a7349d8
Bor3y9 <yousef161247@eng.psu.edu.eg>
2023-10-30 16:38:01 +0200

    Final fixes for add func

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -16,13 +16,13 @@ def add_integer(a, b=98):
         The sum of the two integers.
     """
 
-    if type (a) not in (int, float):
+    if type(a) not in (int, float):
         raise TypeError('a must be an integer')
-    if type (b) not in (int, float):
+    if type(b) not in (int, float):
         raise TypeError('b must be an integer')
     return int(a) + int(b)
+
 
 if __name__ == "__main__":
     import doctest
     doctest.testfile("tests/0-add_integer.txt")
-    
