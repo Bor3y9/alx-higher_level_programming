b6f1030caa7bcb6b2eb5563e7ae33159cb45210b
Bor3y9 <yousef161247@eng.psu.edu.eg>
2024-02-12 16:27:13 +0200

    0x12. JavaScript - Warm up

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-if (process.argv[2] === undefined) console.log('Missing size');
+if (process.argv[2] === undefined || isNaN(process.argv[2])) console.log('Missing size');
 else {
   for (let r = 0; r < Number(process.argv[2]); r++) {
     let row = '';
