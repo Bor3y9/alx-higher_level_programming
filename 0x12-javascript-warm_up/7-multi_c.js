44b2be4897aba0dddd74ea6d5faa1d3f020e3661
Bor3y9 <yousef161247@eng.psu.edu.eg>
2024-02-12 16:15:41 +0200

    0x12. JavaScript - Warm up

---
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -0,0 +1,5 @@
+#!/usr/bin/node
+if (process.argv[2] === undefined) console.log('Missing number of occurrences');
+else {
+  for (let i = 0; i < Number(process.argv[2]); i++) console.log('C is fun');
+}
