0725c7afbe369bbfe14d97d5040101affdc9a42a
Bor3y9 <yousef161247@eng.psu.edu.eg>
2024-02-12 15:33:57 +0200

    0x12. JavaScript - Warm up

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,4 +1,4 @@
 #!/usr/bin/node
 const args = process.argv.length;
-if (args === 0) console.log('No argument');
-else console.log('Arguments found');
+if (args === 2) console.log('No argument');
+else if (args > 2) console.log('Arguments found');
