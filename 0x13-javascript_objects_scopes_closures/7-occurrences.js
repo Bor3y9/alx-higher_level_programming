71b7ae65824267239e5e2ad9441a50674812cd1a
Bor3y9 <yousef161247@eng.psu.edu.eg>
2024-02-14 18:16:03 +0200

    0x13-javascript_objects_scopes_closures

---
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -0,0 +1,7 @@
+exports.nbOccurences = function (list, searchElement) {
+  let cntr = 0;
+  for (let i = 0; i < list.length; i++) {
+    if (list[i] === searchElement) cntr++;
+  }
+  return cntr;
+};
