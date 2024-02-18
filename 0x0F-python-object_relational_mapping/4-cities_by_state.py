836899a3cff0fc8bf11d31acff45c431cd0faeb6
Bor3y9 <yousef161247@eng.psu.edu.eg>
2024-02-18 19:34:15 +0200

    0x0F-python-object_relational_mapping

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -15,7 +15,7 @@ if __name__ == "__main__":
     cur.execute("SELECT `cities`.`id`, `cities`.`name`, `states`.`name`\
                 FROM `cities`\
                 JOIN `states` ON `cities`.`state_id` = `states`.`id`\
-                ORDER BY `cities`.`id`")
+                ORDER BY `cities`.`id` ASC")
     rows = cur.fetchall()
     for row in rows:
         print(row)
