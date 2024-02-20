d0d78ed794845551a2f623b6a81835d767a8cfc7
Bor3y9 <yousef161247@eng.psu.edu.eg>
2024-02-20 19:39:52 +0200

    0x0F-python-object_relational_mapping

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
-"""
-lists all State objects from the database hbtn_0e_6_usa
-"""
+"""lists all State objects from the database hbtn_0e_6_usa"""
 from model_state import Base, State
 from sqlalchemy.orm import sessionmaker, declarative_base
 from sqlalchemy import create_engine
