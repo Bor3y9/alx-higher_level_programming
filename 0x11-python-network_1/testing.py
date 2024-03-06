f537c859b40d3a97e616a19e2a08e5093d429650
Bor3y9 <yousef161247@eng.psu.edu.eg>
2024-03-06 21:15:56 +0200

    0x11-python-network_1

--- 0x11-python-network_1/testing.py
+++ 0x11-python-network_1/testing.py
@@ -1,6 +1,5 @@
-import urllib.request
-req = urllib.request.Request('https://www.python.org/fish.html')
-try:
-    urllib.request.urlopen(req)
-except urllib.error.HTTPError as e:
-    print("Error code: {}".format(e.code))
+import requests
+
+# data1 = {'page':25,'count':3}
+r = requests.get('https://httpbin.org/')
+print(r.content)
