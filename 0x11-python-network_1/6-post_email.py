38f910619e9a0a6424cbd619d2f356d2f2ba4a91
Bor3y9 <yousef161247@eng.psu.edu.eg>
2024-03-06 21:52:09 +0200

    0x11-python-network_1

---
+++ 0x11-python-network_1/6-post_email.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+""" script that post an email """
+import requests
+import sys
+
+if __name__ == "__main__":
+    payload = {'email':sys.argv[2]}
+    r = requests.post(sys.argv[1],payload)
+    print(r.text)
--- 0x11-python-network_1/testing.py
+++ 0x11-python-network_1/testing.py
@@ -2,4 +2,4 @@ import requests
 
 payload = {'username': 'bor3y1', 'password': '12345' }
 r = requests.post("https://httpbin.org/post",data=payload)
-print(r.headers)
+print(r.text)
