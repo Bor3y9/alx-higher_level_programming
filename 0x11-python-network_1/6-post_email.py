11ea9c8cfce6ba63dff35adb3c51d0f6a2f593d0
Bor3y9 <yousef161247@eng.psu.edu.eg>
2024-03-06 21:52:57 +0200

    0x11-python-network_1

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -4,6 +4,6 @@ import requests
 import sys
 
 if __name__ == "__main__":
-    payload = {'email':sys.argv[2]}
-    r = requests.post(sys.argv[1],payload)
+    payload = {'email': sys.argv[2]}
+    r = requests.post(sys.argv[1], payload)
     print(r.text)
