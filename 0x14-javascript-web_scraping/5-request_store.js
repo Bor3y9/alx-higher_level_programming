7ac9a802c755a82da8244465878dfc1ec1dba4fc
Bor3y9 <yousef161247@eng.psu.edu.eg>
2024-04-24 01:44:11 +0200

    fixes

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -3,11 +3,10 @@
 const request = require('request');
 const fs = require('fs');
 
-request(process.argv[2], (err, response, body) => {
-  fs.writeFile(process.argv[3], body, 'utf-8', (err) => {
+request(process.argv[2], function (_err, _res, body) {
+  fs.writeFile(process.argv[3], body, 'utf8', function (err) {
     if (err) {
       console.log(err);
     }
   });
 });
-
